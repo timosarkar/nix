7c956a24a2178c12e7303f65d40ea74245f8dc1e
timsarkar <sartimo10@gmail.com>
2024-10-21 09:53:16 +0200

    added fish shell

--- home.nix
+++ home.nix
@@ -1,17 +1,38 @@
 { config, pkgs, ... }:
 
 let
+  # get current username
   username = if builtins.getEnv "USER" != "" then builtins.getEnv "USER" else "root";
 in
 {
   home.username = username;
+  
+  # set homedir to /root when $username is root
   home.homeDirectory = if username == "root" then "/root" else "/home/${username}";
   home.stateVersion = "23.05";
 
-  home.packages = with pkgs; [];
+  home.packages = with pkgs; [
+    fish
+  ];
+
+  # fish shell
+  programs.fish = {
+    enable = true;
+
+    # aliases
+    shellAliases = {
+      podb = "podman build -t";
+      podr = "podman run -it";
+      nixhome = "nix run nixpkgs#home-manager -- switch --flake ./#$USER";
+      nano = "vim";
+      v = "vim";
+      e = "vim"; 
+    };
+  };
 
   programs.home-manager.enable = true;
 
+  # vim ultisnips
   home.file.".vim/UltiSnips/all.snippets" = {
     text = ''
       snippet lorem "lorem ipsum text"
@@ -21,13 +42,16 @@ in
     force = true;
   };
 
+  # vim
   programs.vim = {
     enable = true;
     settings = {
        relativenumber = true;
        number = true;
        mouse = "a";
-    };
+     };
+
+    # .vimrc
     extraConfig = ''
       set tabstop=2
       set softtabstop=2
@@ -38,6 +62,8 @@ in
       let g:UltiSnipsExpandTrigger="<tab>"
       let g:UltiSnipsSnippetDirectories=["UltiSnips"]
     '';
+
+    # vim plugins
     plugins = with pkgs.vimPlugins; [
        vim-monokai-pro
        ultisnips
