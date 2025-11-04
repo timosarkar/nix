0bb57aeae5145b4d91e29c392f22e283c22f9662
Timo Sarkar <timosarkartimo@gmail.com>
2025-11-04 11:56:06 +0100

    Create flake.nix

---
+++ flakes/cpp/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Simple C++ project using LLVM and CMake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "project";
+          version = "0.1.0";
+
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.cmake ];
+          buildInputs = [ pkgs.llvmPackages_16.llvm pkgs.llvmPackages_16.clang ];
+
+          cmakeFlags = [];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp project $out/bin/
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.cmake
+            pkgs.llvmPackages_16.llvm
+            pkgs.llvmPackages_16.clang
+          ];
+
+          shellHook = ''
+            echo "Welcome to your C++ dev shell with LLVM and CMake!"
+          '';
+        };
+      });
+}
